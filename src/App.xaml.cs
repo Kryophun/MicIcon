38fa12c02f7cab17d4557c366b7922e4bef2f494
Aaron Erlandson <aaroner@microsoft.com>
2020-04-05 17:16:27 -0700

    Skeleton app with icon

---
+++ src/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.Drawing;
+using System.Windows;
+
+namespace MicIcon
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            TaskbarIcon tbi = new TaskbarIcon();
+            tbi.Icon = new Icon(SystemIcons.Exclamation, 40, 40);
+            tbi.ToolTipText = "hello world";
+        }
+    }
+}
