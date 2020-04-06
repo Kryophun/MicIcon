d9ffc6bc83f2206429a65de1844e03234b8a0d99
Aaron Erlandson <aaroner@microsoft.com>
2020-04-06 09:43:55 -0700

    Sigh

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Hardcodet.Wpf.TaskbarNotification;
+using NAudio.CoreAudioApi;
 using System;
 using System.Drawing;
 using System.Windows;
@@ -11,6 +12,10 @@ namespace MicIcon
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NAudio.CoreAudioApi.MMDeviceEnumerator deviceEnum = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+        private NotificationClientImplementation notificationClient;
+        private NAudio.CoreAudioApi.Interfaces.IMMNotificationClient notifyClient;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,6 +25,10 @@ namespace MicIcon
             tbi.Icon = new Icon(SystemIcons.Exclamation, 40, 40);
             tbi.ToolTipText = "hello world";
             tbi.LeftClickCommand = new ShowMessageCommand(this);
+
+            notificationClient = new NotificationClientImplementation();
+            notifyClient = (NAudio.CoreAudioApi.Interfaces.IMMNotificationClient)notificationClient;
+            deviceEnum.RegisterEndpointNotificationCallback(notifyClient);
         }
 
         private void MainWindow_StateChanged(object sender, EventArgs e)
@@ -27,9 +36,6 @@ namespace MicIcon
             if (this.WindowState == WindowState.Minimized)
             {
                 this.Visibility = Visibility.Hidden;
-            } else
-            {
-                MessageBox.Show("New state: " + this.WindowState.ToString());
             }
         }
     }
@@ -43,10 +49,9 @@ namespace MicIcon
 
         public void Execute(object parameter)
         {
-            // MessageBox.Show(parameter.ToString());
-            // MessageBox.Show("Thanks for clicking on me!");
             window.Visibility = Visibility.Visible;
-            // window
+            window.WindowState = WindowState.Normal;
+            window.Activate();
         }
 
         public bool CanExecute(object parameter)
@@ -56,5 +61,50 @@ namespace MicIcon
 
         public event EventHandler CanExecuteChanged;
         private Window window;
+    }
+
+    class NotificationClientImplementation : NAudio.CoreAudioApi.Interfaces.IMMNotificationClient
+    {
+        public void OnDefaultDeviceChanged(DataFlow dataFlow, Role deviceRole, string defaultDeviceId)
+        {
+            //Do some Work
+            Console.WriteLine("OnDefaultDeviceChanged --> {0}", dataFlow.ToString());
+        }
+
+        public void OnDeviceAdded(string deviceId)
+        {
+            //Do some Work
+            Console.WriteLine("OnDeviceAdded -->");
+        }
+
+        public void OnDeviceRemoved(string deviceId)
+        {
+
+            Console.WriteLine("OnDeviceRemoved -->");
+            //Do some Work
+        }
+
+        public void OnDeviceStateChanged(string deviceId, DeviceState newState)
+        {
+            Console.WriteLine("OnDeviceStateChanged\n Device Id -->{0} : Device State {1}", deviceId, newState);
+            MessageBox.Show("Hello!");
+            //Do some Work
+        }
+
+        public NotificationClientImplementation()
+        {
+            //_realEnumerator.RegisterEndpointNotificationCallback();
+            if (System.Environment.OSVersion.Version.Major < 6)
+            {
+                throw new NotSupportedException("This functionality is only supported on Windows Vista or newer.");
+            }
+        }
+
+        public void OnPropertyValueChanged(string deviceId, PropertyKey propertyKey)
+        {
+            //Do some Work
+            //fmtid & pid are changed to formatId and propertyId in the latest version NAudio
+            Console.WriteLine("OnPropertyValueChanged: formatId --> {0}  propertyId --> {1}", propertyKey.formatId.ToString(), propertyKey.propertyId.ToString());
+        }
     }
 }
